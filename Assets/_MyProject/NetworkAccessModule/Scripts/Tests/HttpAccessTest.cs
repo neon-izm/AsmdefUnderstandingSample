acb8dc52b9e81b895f5af704b482d0104eba5c7c
neon-izm <jj.y0sh1da@gmail.com>
2019-12-27 02:48:56 +0900

    Step by Step Guideを終わらせてAssembly Definitionを設定完了したらこうなる

---
+++ Assets/_MyProject/NetworkAccessModule/Scripts/Tests/HttpAccessTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using AsmdefLearning.Networking;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class HttpAccessTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void HttpAccessTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator HttpAccessTestWithEnumeratorPasses()
+        {
+            var go = Resources.Load<GameObject>("MainThreadDispacher");
+            GameObject.Instantiate(go);
+
+            yield return new WaitForSeconds(1f);
+            bool waiting = true;
+            GetHttpStatus.GetHttpStatusTargetUrl("https://httpbin.org", i =>
+            {
+                Assert.AreEqual(200, i);
+
+                waiting = false;
+            });
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            while (waiting)
+            {
+                yield return null;
+            }
+
+            Debug.Log("テスト通りました");
+        }
+    }
+}
